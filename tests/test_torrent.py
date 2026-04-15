4b948dd2019fb1485e918ba18f3a2b08fbca98c9
soareseng <255757380+soareseng@users.noreply.github.com>
2026-04-15 20:00:38 -0300

    add ubuntu.torrent file and update test to load from correct path

--- tests/test_torrent.py
+++ tests/test_torrent.py
@@ -1,11 +1,17 @@
 from src.torrent import Torrent
 
+
 def test_load_from_path():
     torrent = Torrent()
-    torrent.load_from_path("files/ubuntu.torrent")
-    assert torrent.announce_list == ["https://torrent.ubuntu.com/announce", "https://ipv6.torrent.ubuntu.com/announce"]
+    torrent.load_from_path("test/files/ubuntu.torrent")
+    assert torrent.announce_list == [
+        "https://torrent.ubuntu.com/announce",
+        "https://ipv6.torrent.ubuntu.com/announce",
+    ]
     assert torrent.length == 5702520832
     assert torrent.number_of_pieces == 21754
     assert torrent.piece_length == 262144
-    assert torrent.files == [{"path": "ubuntu-25.10-desktop-amd64.iso", "length": 5702520832}]
+    assert torrent.files == [
+        {"path": "ubuntu-25.10-desktop-amd64.iso", "length": 5702520832}
+    ]
     assert torrent.name == "ubuntu-25.10-desktop-amd64.iso"
