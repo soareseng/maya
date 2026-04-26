8e9b69a18d36c363ad904e287c27427894dfc605
soareseng <255757380+soareseng@users.noreply.github.com>
2026-04-26 18:11:39 -0300

    rename message class to messagetype and implement message class for message handling

--- src/peer/message.py
+++ src/peer/message.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class Message(Enum):
+class MessageType(Enum):
     CHOKE = 0
     UNCHOKE = 1
     INTERESTED = 2
@@ -11,3 +11,21 @@ class Message(Enum):
     REQUEST = 6
     PIECE = 7
     CANCEL = 8
+    PORT = 9
+
+
+class Message:
+    def __init__(self, msg_length: int, msg_type: MessageType, payload: bytes | None):
+        self.msg_length = msg_length
+        self.msg_type = msg_type
+        self.payload = payload
+
+    def to_bytes(self) -> bytes:
+        return (
+            self.msg_length.to_bytes(4, "big")
+            + bytes([self.msg_type.value])
+            + (self.payload or b"")
+        )
+
+    def __str__(self):
+        return f"Message(length={self.msg_length}, type={self.msg_type}, payload={self.payload})"
