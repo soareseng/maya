37b0beddbb56f57ce102dec4b7a910d1c7aed01d
soareseng <255757380+soareseng@users.noreply.github.com>
2026-05-31 12:51:31 -0300

    feat: implement MagnetInfo class and parse_magnet_link function for magnet link parsing

---
+++ src/magnet/magnet.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+from urllib.parse import urlparse, unquote
+
+@dataclass
+class MagnetInfo:
+    name: str
+    tracker_urls: list[str]
+    info_hash: str
+
+
+def parse_magnet_link(magnet_link: str) -> MagnetInfo:
+    """
+    Parses a magnet link and extracts the name, tracker URL, and info hash.
+    
+    Args:
+        magnet_link (str): The magnet link to parse.
+    Returns:
+        MagnetInfo: An object containing the name, tracker URL, and info hash.
+    """
+    params = magnet_link.split('?')[1].split('&')
+    info_hash = None
+    name = None
+    tracker_urls = []
+    for param in params:
+        if param.startswith('xt=urn:btih:'):
+            info_hash = param.split('xt=urn:btih:')[1]
+            if len(info_hash) != 40:
+                raise ValueError(f"Invalid info hash length: {len(info_hash)}. Expected 40 characters.")
+        elif param.startswith('dn='):
+            name = param.split('dn=')[1]
+        elif param.startswith('tr='):
+            tracker_url = unquote(urlparse(param.split('tr=')[1]).geturl())
+            tracker_urls.append(tracker_url)
+    return MagnetInfo(name=name, tracker_urls=tracker_urls, info_hash=info_hash)
+    
---
+++ tests/test_magnet.py
@@ -0,0 +1,27 @@
+from src.magnet.magnet import parse_magnet_link
+
+def test_parse_magnet_link():
+    magnet_link = "magnet:?xt=urn:btih:6f5680d2861d631a7c9621ccac1d893018605274&dn=pop-os_24.04_amd64_generic_24.iso&tr=udp%3A%2F%2Ffosstorrents.com%3A6969%2Fannounce&tr=http%3A%2F%2Ffosstorrents.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker-udp.gbitt.info%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.io%3A6969%2Fannounce&ws=https%3A%2F%2Fiso.pop-os.org%2F24.04%2Famd64%2Fgeneric%2F24%2Fpop-os_24.04_amd64_generic_24.iso&ws=http%3A%2F%2Ffosstorrents.com%2Fdirect-links%2Fpop-os_24.04_amd64_generic_24.iso"
+    magnet = parse_magnet_link(magnet_link)
+    assert magnet.name == "pop-os_24.04_amd64_generic_24.iso"
+    assert magnet.info_hash == "6f5680d2861d631a7c9621ccac1d893018605274"
+    assert len(magnet.info_hash) == 40
+    assert magnet.tracker_urls == [
+        "udp://fosstorrents.com:6969/announce",
+        "http://fosstorrents.com:6969/announce",
+        "udp://tracker.opentrackr.org:1337/announce",
+        "udp://tracker.torrent.eu.org:451/announce",
+        "udp://tracker-udp.gbitt.info:80/announce",
+        "udp://open.demonii.com:1337/announce",
+        "udp://open.stealth.si:80/announce",
+        "udp://exodus.desync.com:6969/announce",
+        "udp://tracker.theoks.net:6969/announce",
+        "udp://opentracker.io:6969/announce"
+    ]
+
+def test_parse_magnet_link_invalid_info_hash():
+    magnet_link = "magnet:?xt=urn:btih:invalid_info_hash&dn=example_file.txt&tr=udp%3A%2F%2Ftracker.example.com%3A6969%2Fannounce"
+    try:
+        parse_magnet_link(magnet_link)
+    except ValueError as e:
+        assert str(e) == "Invalid info hash length: 17. Expected 40 characters."
